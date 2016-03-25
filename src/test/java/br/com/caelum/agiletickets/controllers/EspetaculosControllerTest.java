4959dc2d794257cf883f0ba16096bc8b95562570
alexandregama <alexandre.gama.lima@gmail.com>
2016-03-25 18:30:25 -0300

    Adiciona teste para a inclusao da sessao no result

--- src/test/java/br/com/caelum/agiletickets/controllers/EspetaculosControllerTest.java
+++ src/test/java/br/com/caelum/agiletickets/controllers/EspetaculosControllerTest.java
@@ -1,12 +1,19 @@
 package br.com.caelum.agiletickets.controllers;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
 import java.math.BigDecimal;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import br.com.caelum.agiletickets.domain.Agenda;
 import br.com.caelum.agiletickets.domain.DiretorioDeEstabelecimentos;
@@ -18,26 +25,23 @@ import br.com.caelum.vraptor.util.test.MockResult;
 import br.com.caelum.vraptor.util.test.MockValidator;
 import br.com.caelum.vraptor.validator.ValidationException;
 import br.com.caelum.vraptor.validator.Validator;
-import static org.hamcrest.Matchers.is;
 
-import static org.junit.Assert.assertThat;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
-import static org.mockito.Mockito.when;
-
+@RunWith(MockitoJUnitRunner.class)
 public class EspetaculosControllerTest {
 
-	private @Mock Agenda agenda;
-	private @Mock DiretorioDeEstabelecimentos estabelecimentos;
-	private @Spy Validator validator = new MockValidator();
-	private @Spy Result result = new MockResult();
+	@Mock
+	private Agenda agenda;
+	@Mock
+	private DiretorioDeEstabelecimentos estabelecimentos;
+	@Spy 
+	private Validator validator = new MockValidator();
+	@Spy
+	private Result result = new MockResult();
 	
 	private EspetaculosController controller;
 
 	@Before
-	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
+	public void setup() {
 		controller = new EspetaculosController(result, validator, agenda, estabelecimentos);
 	}
 
@@ -100,6 +104,17 @@ public class EspetaculosControllerTest {
 		controller.reserva(1234l, 5);
 
 		verifyZeroInteractions(result);
+	}
+	
+	@Test
+	public void deveriaIncluirASessaoNoResultQuandoUsuarioBuscarUmaSessaoEElaExistir() throws Exception {
+		Sessao sessao = new Sessao();
+		
+		when(agenda.sessao(1L)).thenReturn(sessao);
+		
+		controller.sessao(1L);
+		
+		verify(result).include("sessao", sessao);
 	}
 
 	@Test
